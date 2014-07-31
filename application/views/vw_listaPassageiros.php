ed62df80127224ade0bf0ee9a009a20a52117f64
Marciso Gonzalez <marciso.gonzalez@gmail.com>
2014-07-30 23:47:59 -0400

    correção da listagem de passageiros

--- application/views/vw_listaPassageiros.php
+++ application/views/vw_listaPassageiros.php
@@ -21,10 +21,21 @@
                     <div class="col-lg-12">
                         <table>
                             <tr>
+                                <?php
+                                $query = $this->db->query("SELECT * FROM tb_reservs
+                                                JOIN tb_tour on tb_tour.id_tour=tb_reservs.id_tour
+                                                JOIN tb_clients on tb_clients.id_clients=tb_reservs.id_client
+                                                JOIN tb_cars on tb_tour.id_car=tb_cars.id_cars
+                                                WHERE tb_reservs.id_tour=".$_GET['id']." ORDER BY tb_reservs.nr_poltrona");
+                                foreach ($query->result() as $row) {
+                                    $result=$row;
+                                }
+                                $data_saida = implode("/", array_reverse(explode("-", $result->data_saida)));
+                                ?>
                                 <td><img src="<?=base_url()?>img/logo_relatorio.png"></td>
                                 <td>Pantanal Sul Turismo (67) 3351-2520 - Campo Grande, MS <br>
-                                    Destino: <small>Nome do onibus</small> - Data de Saída:<small>27/01/1987</small><br>
-                                    Onibus: <small>Scania</small><br>
+                                    Destino: <small><?=$result->destino_ultv?></small> - Data de Saída: <small><?=$data_saida?></small><br>
+                                    Onibus: <small><?=$result->modelo?></small><br>
                                     Assitanura Responsavel:________________________
                                 </td>
                             </tr>
@@ -35,15 +46,11 @@
                 <table class="table table-bordered">
                     <thead>
                         <tr>
-                            <th>Nr.</th>
+                            <th width="75px">Nr.</th>
                             <th>Informaçoes</th>                          
                         </tr>
                     </thead>
                     <?php
-                    $query = $this->db->query("SELECT * FROM tb_reservs
-                                                JOIN tb_tour on tb_tour.id_tour=tb_reservs.id_tour
-                                                JOIN tb_clients on tb_clients.id_clients=tb_reservs.id_client
-                                                WHERE tb_reservs.id_tour=".$_GET['id']." ORDER BY tb_reservs.nr_poltrona");
                     foreach ($query->result() as $row) {
                         $data_nascimento = implode("/", array_reverse(explode("-", $row->data_nascimento)));
                         ?>
