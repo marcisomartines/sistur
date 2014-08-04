d66c5cd6b3c250c3e72ca58db3896e6d2b52a805
Marciso Gonzalez <marciso.gonzalez@gmail.com>
2014-08-04 11:32:53 -0400

    onibus

---
+++ application/views/vw_relatorioOnibus.php
@@ -0,0 +1,147 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <meta name="description" content="">
+        <meta name="author" content="Marciso Gonzalez Martines">
+
+        <title>Pantanal Sul - Turismo</title>
+        <link href="<?= base_url() ?>css/bootstrap.css" rel="stylesheet">
+        <link href="<?= base_url() ?>css/sb-admin.css" rel="stylesheet">
+        <link rel="stylesheet" href="<?= base_url() ?>font-awesome/css/font-awesome.min.css">
+        <link rel="stylesheet" href="http://cdn.oesmith.co.uk/morris-0.4.3.min.css">
+        <script type="text/javascript">
+            $().ready(function() {
+                $("#course").autocomplete("home/autoComplete", {
+                    width: 260,
+                    matchContains: true,
+                    selectFirst: false
+                });
+            });
+        </script>
+    </head>
+
+    <body>
+
+        <div id="wrapper">
+
+            <!-- barra lateral -->
+            <nav class="navbar navbar-inverse navbar-fixed-top" role="navigation">
+
+                <div class="navbar-header">
+                    <button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-ex1-collapse">
+                        <span class="sr-only">Toggle navigation</span>
+                        <span class="icon-bar"></span>
+                        <span class="icon-bar"></span>
+                        <span class="icon-bar"></span>
+                    </button>
+                    <a class="navbar-brand" href="">Pantanal Sul Turismo</a>
+                </div>
+
+                <div class="collapse navbar-collapse navbar-ex1-collapse">
+                    <ul class="nav navbar-nav side-nav">
+                        <li><a href="<?php echo base_url() . "index.php/home/" ?>"><i class="fa fa-dashboard"></i> Geral</a></li>
+                        <li class="active"><a href="<?php echo base_url() . "index.php/home/reserva" ?>"><i class="fa fa-ticket"></i> Reserva</a></li>
+                        <li><a href="<?php echo base_url() . "index.php/home/cliente" ?>"><i class="fa fa-users "></i> Cliente</a></li>
+                        <li><a href="<?php echo base_url() . "index.php/home/agenda" ?>"><i class="fa fa-calendar"></i> Agendamento</a></li>
+                        <li><a href="<?php echo base_url() . "index.php/home/onibus" ?>"><i class="fa fa-truck"></i> Ônibus</a></li>
+                        <li><a href="<?php echo base_url() . "index.php/home/viagem" ?>"><i class="fa fa-tasks"></i> Destino</a></li>
+                        <li><a href="<?php echo base_url() . "index.php/home/motorista" ?>"><i class="fa fa-car"></i> Motorista</a></li>
+                        <li><a href="<?php echo base_url() . "index.php/home/usuario" ?>"><i class="fa fa-user"></i> Usuário</a></li>
+                        <li class="dropdown">
+                            <a href="#" class="dropdown-toggle" data-toggle="dropdown"><i class="fa fa-bar-chart-o"></i> Relatórios <b class="caret"></b></a>
+                            <ul class="dropdown-menu">
+                                <li><a href="<?php echo base_url() . "index.php/home/relatorioCliente" ?>"> Clientes</a></li>
+                                <li><a href="<?php echo base_url() . "index.php/home/relatorioOnibus" ?>"> Ônibus</a></li>
+                                <li><a href="<?php echo base_url() . "index.php/home/relatorioViagem" ?>"> Viagem</a></li>
+                            </ul>
+                        </li>
+                    </ul>
+                    <!-- Menu superior alinhado a direita-->
+                    <ul class="nav navbar-nav navbar-right navbar-user">
+                        <?php
+                        $this->db->where('nome_user', $this->session->userdata('nome'));
+                        $query = $this->db->get('tb_users');
+                        $query = $query->result();
+                        ?>
+                        <li class="dropdown user-dropdown">
+                            <a href="#" class="dropdown-toggle" data-toggle="dropdown"><i class="fa fa-user"></i> <?= $query[0]->nome_user ?> <b class="caret"></b></a>
+                            <ul class="dropdown-menu">
+                                <li><a href="#"><i class="fa fa-user"></i> Perfil</a></li>
+                                <li><a href="#"><i class="fa fa-gear"></i> Configurações</a></li>
+                                <li class="divider"></li>
+                                <li><a href="<?php echo base_url() . "index.php/home/logout" ?>"><i class="fa fa-power-off"></i> Sair</a></li>
+                            </ul>
+                        </li>
+                    </ul>
+                </div><!-- /.navbar-collapse -->
+            </nav>
+            <!--fim do menu superior alinhado a direita-->
+            <div id="page-wrapper">
+                <div class="row">
+                    <div class="col-lg-12">
+                        <h1>Relatório <small>Viagem</small></h1>
+                        <!--                        <ol class="breadcrumb">
+                                                    <li class="active"><i class="fa fa-ticket"></i> Reserva</li>
+                                                </ol>-->
+                    </div>
+                </div><!-- /.row -->
+                <div class="controls">
+                    <table>
+                        <tr>
+                            <td><?php
+                                $query = $this->db->get('tb_clients');
+                                $opcao[] = '';
+                                echo form_label('Destino: ');
+                                ?>
+                            </td>
+                            <td>                    
+                                <select name="id_client" id="id_client" class="form-control input-sm">
+                                    <?php
+                                    foreach ($query->result() as $bus) {
+                                        echo "<option value=" . $bus->id_clients . ">" . $bus->nome . "</option>";
+                                    }
+                                    ?>
+                                </select>
+                            </td>
+                            <td> <label for="data_inicial">Periodo Inicial: </label></td><td><input type="text" id="data_inicio" class="form-control input-sm"></td><td><label for="data_final"> Periodo Final: </label></td><td><input type="text" id="data_final" class="form-control input-sm"></td>
+                            <td><?php
+                                $query = $this->db->get('tb_viagem');
+                                $opcao[] = '';
+                                echo form_label('Destino: ');
+                                ?></td>
+                            <td>                    
+                                <select name="id_viagem" id="id_viagem" class="form-control input-sm">
+                                    <?php
+                                    foreach ($query->result() as $bus) {
+                                        echo "<option value=" . $bus->id_viagem . ">" . $bus->destino . "</option>";
+                                    }
+                                    ?>
+                                </select>
+                            </td>
+                        </tr>
+                    </table>
+                    <br>
+                    <a class="btn btn-primary" href="" onClick="window.open('<?php echo base_url() . "index.php/home/listaRelatorioCliente?id=&data_saida=&data_retorno&destino=" . $this->input->post('id_tour') ?>', 'Janela', 'toolbar=no,location=no,directories=no,status=no,menubar=no,scrollbars=yes,resizable=no,width=900,height=800,left=0,top=0');
+                            return false;"><i class="fa fa-list"></i> Gerar Relatório</a>
+                </div>
+                <div id="relatorio" class=" row-fluid">
+                    <!--reserva vai ser colocada aqui-->
+                </div>
+            </div><!-- /#page-wrapper -->
+        </div><!-- /#wrapper -->
+        <!-- JavaScript -->
+        <script src="<?= base_url() ?>js/jquery-1.10.2.js"></script>
+        <script src="<?= base_url() ?>js/bootstrap.js"></script>
+        <!-- Page Specific Plugins -->
+        <script src="http://cdnjs.cloudflare.com/ajax/libs/raphael/2.1.0/raphael-min.js"></script>
+        <script src="http://cdn.oesmith.co.uk/morris-0.4.3.min.js"></script>
+        <script src="<?= base_url() ?>js/morris/chart-data-morris.js"></script>
+        <script src="<?= base_url() ?>js/tablesorter/jquery.tablesorter.js"></script>
+        <script src="<?= base_url() ?>js/tablesorter/tables.js"></script>
+        <script src="<?= base_url() ?>js/funcao.js"></script>
+        <script src="<?= base_url() ?>js/jquery-ui.js"></script>
+        <script src="<?= base_url() ?>js/jquery.autocomplete.js"></script>
+    </body>
+</html>
