401d5380c52db523e1e0d1737f0a9cc7dff92fbd
Marciso Gonzalez <marciso.gonzalez@gmail.com>
2015-01-01 16:45:18 -0300

    correcting the passengers list

--- application/views/vw_listaPassageiros.php
+++ application/views/vw_listaPassageiros.php
@@ -1,3 +1,8 @@
+<?php 
+$this->db->where('nome_user', $this->session->userdata('nome'));
+$query = $this->db->get('tb_users');
+$query = $query->result();
+?>
 <!DOCTYPE html>
 <html lang="en">
     <head>
@@ -22,11 +27,12 @@
                     <table>
                         <tr>
                             <?php
-                            $query = $this->db->query("SELECT * FROM tb_reservs
-                                                JOIN tb_tour on tb_tour.id_tour=tb_reservs.id_tour
-                                                JOIN tb_clients on tb_clients.id_clients=tb_reservs.id_client
-                                                JOIN tb_cars on tb_tour.id_car=tb_cars.id_cars
-                                                WHERE tb_reservs.id_tour=" . $_GET['id'] . " ORDER BY tb_reservs.nr_poltrona");
+                            $query = $this->db->query("SELECT tb_reservs.destino_ultv,tb_cars.codigo, tb_tour.data_saida,tb_cars.nr_poltrona 
+                                                        FROM tb_reservs
+                                                        JOIN tb_tour on tb_tour.id_tour=tb_reservs.id_tour
+                                                        JOIN tb_clients on tb_clients.id_clients=tb_reservs.id_client
+                                                        JOIN tb_cars on tb_tour.id_car=tb_cars.id_cars
+                                                        WHERE tb_reservs.id_tour=" . $_GET['id']);
                             if ($query->num_rows() > 0) {
                                 foreach ($query->result() as $row) {
                                     $result = $row;
@@ -49,28 +55,41 @@
                 <thead>
                     <tr>
                         <th width="75px">Nr.</th>
-                        <th>Informaçoes</th>                          
+                        <th>Informa&ccedil;&otilde;es</th>                          
                     </tr>
                 </thead>
                 <?php
-                $query = $this->db->query("SELECT tb_reservs.nr_poltrona,tb_clients.nome,tb_clients.celular,tb_clients.loc_embarque as embarquec, 
-                                                  tb_reservs.loc_embarque as embarquer,tb_reservs.tipo,tb_clients.rg,tb_clients.telefone
+                $poltrona=1;
+                $count=0;//contador
+                while($count < $result->nr_poltrona){
+                    $query = $this->db->query("SELECT tb_reservs.nr_poltrona,tb_clients.nome,tb_clients.celular,tb_clients.loc_embarque as embarquec, 
+                                                  tb_reservs.loc_embarque as embarquer,tb_reservs.tipo,tb_clients.rg,tb_clients.telefone,
+                                                  tb_cars.nr_poltrona as total_poltronas
                                                 FROM tb_reservs
                                                 JOIN tb_tour on tb_tour.id_tour=tb_reservs.id_tour
                                                 JOIN tb_clients on tb_clients.id_clients=tb_reservs.id_client
                                                 JOIN tb_cars on tb_tour.id_car=tb_cars.id_cars
-                                                WHERE tb_reservs.id_tour=" . $_GET['id'] . " ORDER BY tb_reservs.nr_poltrona");
-                $poltrona=1;//contador
-                foreach ($query->result() as $row) {
-                    ?>
-                    <tr>
+                                                WHERE tb_reservs.id_tour=" . $_GET['id'] . " AND tb_reservs.nr_poltrona= ".$poltrona." ORDER BY tb_reservs.nr_poltrona");
+                    if($query->num_rows() >0){
+                        foreach($query->result() as $row){
+                        ?>
+                        <tr>
                         <td><?php if (!empty($row->nr_poltrona)) echo $row->nr_poltrona; ?></td>
-                        <td><?php if (!empty($row->nome)) echo $row->nome; ?> / RG:<?php if (!empty($row->rg)) echo $row->rg; ?> / Cel.:<?php if (!empty($row->celular)) echo $row->celular; ?> / Tel.: <?php if (!empty($row->telefone)) echo $row->telefone; ?> / Loc. Embarque:<?php if (!empty($row->embarquer)) echo $row->embarquer;
+                        <td><?php if (!empty($row->nome)) echo $row->nome; ?> / RG:<?php if (!empty($row->rg)) echo $row->rg; ?> / Cel.:<?php if (!empty($row->celular)) echo $row->celular; ?> / Loc. Embarque:<?php if (!empty($row->embarquer)) echo $row->embarquer;
                                 else if (!empty($row->embarquec)) echo $row->embarquec; ?> <?php if($row->tipo=='i') echo '<span class="badge">IDA</span>'; if($row->tipo=='v') echo '<span class="badge">VOLTA</span>'; ?></td>
                     </tr>
-                    <?php
+                        <?php
+                        }
+                    }else{
+                       echo "<tr>";
+                       echo "<td>{$poltrona}</td>";
+                       echo "<td></td>";
+                       echo "</tr>";
+                    }
+                    $count++;
+                    $poltrona++;
                 }
-                ?>
+                 ?>
             </table>
         </div><!-- /#page-wrapper -->
         <!--        </div> /#wrapper -->
